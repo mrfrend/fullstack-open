e0eb9f682f321c3a44465114c3e4348190cb9607
Ivan Deshchenko <ideshchenko@yandex.ru>
2026-07-16 11:48:17 +0300

    feat: add get_users

--- part4/blog/controllers/users.js
+++ part4/blog/controllers/users.js
@@ -1,6 +1,12 @@
 const usersRouter = require("express").Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+
+usersRouter.get("/", async (request, response) => {
+  const users = await User.find({});
+  response.json(users);
+});
+
 usersRouter.post("/", async (request, response) => {
   const { username, password, name } = request.body;
   if (!(username && password && name)) {
