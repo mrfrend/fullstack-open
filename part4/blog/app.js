ea5c5dd2ff7087fd4f807291c636b5df1071a6e2
Ivan Deshchenko <ideshchenko@yandex.ru>
2026-07-19 15:37:43 +0300

    feat: add token-based authentication

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -9,6 +9,7 @@ const {
 } = require("./utils/middleware");
 const blogsRouter = require("./controllers/blogs");
 const usersRouter = require("./controllers/users");
+const loginRouter = require("./controllers/login");
 
 const app = express();
 
@@ -28,6 +29,7 @@ app.use(requestLogger);
 
 app.use("/api/blogs", blogsRouter);
 app.use("/api/users", usersRouter);
+app.use("/api/login", loginRouter);
 
 app.use(unknownEndpoint);
 app.use(errorHandler);
